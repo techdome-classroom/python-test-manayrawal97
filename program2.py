c2e87a675e22f0beee67ca2a241b2e861d58b1e3
manayrawal97 <83336564+manayrawal97@users.noreply.github.com>
2024-05-09 13:20:26 +0530

    Manay Update program2.py
    added the function

--- program2.py
+++ program2.py
@@ -1,5 +1,40 @@
-def decode_message( s: str, p: str) -> bool:
+import unittest
+from program2 import decode_message
 
-# write your code here
-  
+def decode_message(s: str, p: str) -> bool:
+    if len(s) != len(p):
         return False
+
+    i, j = 0, 0
+    while i < len(s) and j < len(p):
+        if p[j] == '?' or p[j] == s[i]:
+            i += 1
+            j += 1
+        elif p[j] == '*':
+            if j == len(p) - 1:
+                return True
+            while i < len(s) and s[i] != p[j+1]:
+                i += 1
+            j += 1
+        else:
+            return False
+
+    return i == len(s) and j == len(p)
+
+
+class TestDecoder(unittest.TestCase):
+    def test_case1(self):
+        self.assertEqual(decode_message("aa", "a"), False)
+
+    def test_case2(self):
+        self.assertEqual(decode_message("a", "*"), True)
+
+    def test_case3(self):
+        self.assertEqual(decode_message("cb", "?a"), False)
+
+    def test_case4(self):
+        self.assertEqual(decode_message("abc", "?b?"), True)
+
+if __name__ == '__main__':
+    unittest.main()
+
